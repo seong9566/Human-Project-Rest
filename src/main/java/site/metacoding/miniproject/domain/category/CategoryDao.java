f0fa60bba70160ef513ae4e5674f1d2857e12cb6
Hyerinism:) <112357320+Jo-HyeRin@users.noreply.github.com>
2022-11-01 17:29:06 +0900

    [조혜린]이력서 작성 기능 완료(#8)
    * 형상맞추기
    
    * 이력서 작성 완료
    
    * 이력서 작성 테스트까지 완료
    
    Co-authored-by: seong9566 <112357327+seong9566@users.noreply.github.com>

--- src/main/java/site/metacoding/miniproject/domain/category/CategoryDao.java
+++ src/main/java/site/metacoding/miniproject/domain/category/CategoryDao.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 public interface CategoryDao {
 	public void insert(Category category);
+
 	public List<Category> findAll();
+
 	public Category findById(Integer categoryId);
+
 	public void update(Category category);
+
 	public void deleteById(Integer categoryId);
-	
-	public void jobPostingUpdate(Category category);	
+
+	public void jobPostingUpdate(Category category);
 
 }
