b38361648140d93d1f073b37252b39c16c9f5fec
sopu55555 <sopu55555@naver.com>
2022-11-04 20:14:00 +0900

    커밋

--- src/test/java/site/metacoding/miniproject/web/CompanyApiControllerTest.java
+++ src/test/java/site/metacoding/miniproject/web/CompanyApiControllerTest.java
@@ -1,74 +1,68 @@
 package site.metacoding.miniproject.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.extern.slf4j.Slf4j;
+import site.metacoding.miniproject.config.MyBatisConfig;
 import site.metacoding.miniproject.domain.company.Company;
 import site.metacoding.miniproject.domain.company.CompanyDao;
 import site.metacoding.miniproject.domain.personal.PersonalDao;
 import site.metacoding.miniproject.domain.users.UsersDao;
-import site.metacoding.miniproject.dto.user.UserRespDto.SignCompanyDto;
-import site.metacoding.miniproject.dto.user.UserRespDto.SignedDto;
 import site.metacoding.miniproject.utill.SHA256;
-import site.metacoding.miniproject.web.dto.request.etc.LoginDto;
 
+@Slf4j
 @ActiveProfiles("test")
-@Sql("classpath:truncate.sql,classpath:test.sql")
-@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@Import(MyBatisConfig.class)
+@Sql("classpath:testdatabase.sql")
 public class CompanyApiControllerTest {
-
     private static final String APPLICATION_JSON = "application/json; charset=utf-8";
+
     @Autowired
     private MockMvc mvc;
 
     @Autowired
     private ObjectMapper om;
 
+    private MockHttpSession session;
+
+    private static HttpHeaders headers;
+
     @Autowired
     private CompanyDao companyDao;
-    @Autowired
-    private UsersDao usersDao;
-    @Autowired
-    private LoginDto loginDto;
-    @Autowired
-    private SHA256 sha256;
-    @Autowired
-    private PersonalDao personalDao;
-    private MockHttpSession session;
+
+    @BeforeAll // 선언시 static으로 선언해야한다. - container에 띄우기 위해 사용한다.
+    public static void init() {
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
 
     @BeforeEach
     public void sessionInit() {
         session = new MockHttpSession();
-        SignCompanyDto signCompanyDto = new SignCompanyDto();
-        signCompanyDto.setCompanyId(4);
-        SignedDto<?> signedDto = new SignedDto<>(1, "company01", signCompanyDto);
-        session.setAttribute("principal", signedDto);
-    }
-
-    @BeforeEach
-    public void sessionconpany() {
-        Company company = Company.builder().companyName("박동훈")
-                .companyEmail("sopu55555@naver.com").companyPhoneNumber("01024102957")
-                .companyPicture("ddd").build();
-        session.setAttribute("sessioncompany", company);
+        Company company = Company.builder().companyId(1).companyName("박동훈").companyEmail("sopu55555@naver.com")
+                .companyPhoneNumber("01024102957")
+                .companyAddress("우리집").companyPicture("ddd").build();
     }
 
     @Test
@@ -76,11 +70,13 @@ public class CompanyApiControllerTest {
         // given
 
         // when
-        ResultActions resultActions = mvc.perform(get("/s/api/company/detail").accept(APPLICATION_JSON));
+        ResultActions resultActions = mvc
+                .perform(get("/s/api/company/detail").session(session)
+                        .contentType("application/json; charset=utf-8").accept(APPLICATION_JSON));
+
         // then
         MvcResult mvcResult = resultActions.andReturn();
         System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
-        resultActions.andExpect(status().isOk());
     }
 
 }
--- src/test/java/site/metacoding/miniproject/web/UsersApiControllerTest.java
+++ src/test/java/site/metacoding/miniproject/web/UsersApiControllerTest.java
@@ -36,36 +36,35 @@ import site.metacoding.miniproject.dto.user.UserRespDto.SignedDto;
 public class UsersApiControllerTest {
 
     private static final String APPLICATION_JSON = "application/json; charset=utf-8";
-    
+
     @Autowired
     private MockMvc mvc;
 
     @Autowired
-	private ObjectMapper om;
+    private ObjectMapper om;
 
     private MockHttpSession session;
 
     private static HttpHeaders headers;
 
     @BeforeAll // 선언시 static으로 선언해야한다. - container에 띄우기 위해 사용한다.
-	public static void init() {
-		headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-	}
-	
-	@BeforeEach// test메서드 진입전에 트랜잭션 발동
-    public void sessionInit() {
-        session = new MockHttpSession();
-		SignPersonalDto signPersonalDto = new SignPersonalDto();
-		signPersonalDto.setPersonalId(1);
-		SignedDto<?> signedDto = new SignedDto<>(1, "testuser", signPersonalDto);
-		session.setAttribute("principal", signedDto);
+    public static void init() {
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
+    @BeforeEach // test메서드 진입전에 트랜잭션 발동
+    public void sessionInit() {
+        session = new MockHttpSession();
+        SignPersonalDto signPersonalDto = new SignPersonalDto();
+        signPersonalDto.setPersonalId(1);
+        SignedDto<?> signedDto = new SignedDto<>(1, "testuser", signPersonalDto);
+        session.setAttribute("principal", signedDto);
+    }
 
     @Test
     public void joinPersonal_test() throws Exception {
-        //given
+        // given
         PersonalJoinDto joinDto = new PersonalJoinDto();
         joinDto.setLoginId("user3");
         joinDto.setLoginPassword("Qwer1234!!!");
@@ -74,44 +73,42 @@ public class UsersApiControllerTest {
         joinDto.setPersonalName("testUsername");
         joinDto.setPersonalAddress("testAddress");
         joinDto.setPersonalEducation("test");
-
         String body = om.writeValueAsString(joinDto);
 
-        //when
+        // when
         ResultActions resultActions = mvc
                 .perform(post("/join/personal").content(body)
                         .contentType("application/json; charset=utf-8").accept(APPLICATION_JSON));
-        //then
+        // then
         MvcResult mvcResult = resultActions.andReturn();
         log.debug("디버그 :  개인 회원가입 - " + mvcResult.getResponse().getContentAsString());
-        //assertEquals(null, joinDto);
+        // assertEquals(null, joinDto);
 
     }
 
-    @Test
-    public void joinPersonal_test2() throws Exception {
-        //given
-        PersonalJoinDto joinDto = new PersonalJoinDto();
-        joinDto.setLoginId("user3");
-        joinDto.setLoginPassword("Qwer1234!!!");
-        joinDto.setPersonalPhoneNumber("000-1111-4444");
-        joinDto.setPersonalEmail("example@example.com");
-        joinDto.setPersonalName("testUsername");
-        joinDto.setPersonalAddress("testAddress");
-        joinDto.setPersonalEducation("test");
+    // @Test
+    // public void joinPersonal_test2() throws Exception {
+    // // given
+    // PersonalJoinDto joinDto = new PersonalJoinDto();
+    // joinDto.setLoginId("user3");
+    // joinDto.setLoginPassword("Qwer1234!!!");
+    // joinDto.setPersonalPhoneNumber("000-1111-4444");
+    // joinDto.setPersonalEmail("example@example.com");
+    // joinDto.setPersonalName("testUsername");
+    // joinDto.setPersonalAddress("testAddress");
+    // joinDto.setPersonalEducation("test");
 
-        String body = om.writeValueAsString(joinDto);
+    // String body = om.writeValueAsString(joinDto);
 
-        //when
-        ResultActions resultActions = mvc
-                .perform(post("/join/personal").content(body)
-                        .contentType("application/json; charset=utf-8").accept(APPLICATION_JSON));
-        //then
-        MvcResult mvcResult = resultActions.andReturn();
-        log.debug("디버그 :  개인 회원가입 - " + mvcResult.getResponse().getContentAsString());
-        //assertEquals(null, joinDto);
+    // // when
+    // ResultActions resultActions = mvc
+    // .perform(post("/join/personal").content(body)
+    // .contentType("application/json; charset=utf-8").accept(APPLICATION_JSON));
+    // // then
+    // MvcResult mvcResult = resultActions.andReturn();
+    // log.debug("디버그 : 개인 회원가입 - " + mvcResult.getResponse().getContentAsString());
+    // // assertEquals(null, joinDto);
 
-    }
-
+    // }
 
 }
