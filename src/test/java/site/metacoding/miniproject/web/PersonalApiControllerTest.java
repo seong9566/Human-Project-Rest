7c7a562f314f571ac51a2f02d09ffbfacab16141
Jo-HyeRin <sayhyelin12@naver.com>
2022-11-07 16:24:41 +0900

    JUNIT 이력서상세보기 완료

--- src/test/java/site/metacoding/miniproject/web/PersonalApiControllerTest.java
+++ src/test/java/site/metacoding/miniproject/web/PersonalApiControllerTest.java
@@ -1,11 +1,37 @@
 package site.metacoding.miniproject.web;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpMethod;
+import org.springframework.mock.web.MockCookie;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import site.metacoding.miniproject.dto.resumes.ResumesReqDto.ResumesInsertReqDto;
+import site.metacoding.miniproject.dto.user.UserRespDto.SignPersonalDto;
+import site.metacoding.miniproject.dto.user.UserRespDto.SignedDto;
+import site.metacoding.miniproject.service.personal.PersonalService;
+import site.metacoding.miniproject.utill.JWTToken.CreateJWTToken;
+import site.metacoding.miniproject.utill.SHA256;
 
 @ActiveProfiles("test")
 @Sql("classpath:truncate.sql")
@@ -14,4 +40,101 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 public class PersonalApiControllerTest {
 
+    private static final String APPLICATION_JSON = "application/json; charset=utf-8";
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private SHA256 sha256;
+
+    @Autowired
+    private ResourceLoader loader;
+
+    @Autowired
+    private PersonalService personalService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private MockCookie mockCookie;
+
+    private MockHttpSession session;
+
+    @BeforeEach
+    public void sessionInit() {
+        session = new MockHttpSession();
+        SignPersonalDto signPersonalDto = new SignPersonalDto();
+        signPersonalDto.setPersonalId(1);
+        SignedDto<?> signedDto = new SignedDto<>(1, "testusers1", signPersonalDto);
+
+        session.setAttribute("principal", signedDto);
+
+        String JwtToken = CreateJWTToken.createToken(signedDto);
+        mockCookie = new MockCookie("Authorization", JwtToken);
+    }
+
+    @AfterEach
+    public void sessionClear() {
+        session.clearAttributes();
+    }
+
+    @Test
+    public void insertResumes_test() throws Exception {
+        // given
+        ResumesInsertReqDto resumesInsertReqDto = new ResumesInsertReqDto();
+        resumesInsertReqDto.setResumesTitle("이력서제목1");
+        resumesInsertReqDto.setResumesIntroduce("자기소개1");
+        resumesInsertReqDto.setResumesPlace("부산경남");
+        resumesInsertReqDto.setCategoryFrontend(true);
+        resumesInsertReqDto.setCategoryBackend(true);
+        resumesInsertReqDto.setCategoryDevops(true);
+        resumesInsertReqDto.setOneYearLess(true);
+        resumesInsertReqDto.setTwoYearOver(false);
+        resumesInsertReqDto.setThreeYearOver(false);
+        resumesInsertReqDto.setFiveYearOver(false);
+        resumesInsertReqDto.setPortfolioFile("포트폴리오파일");
+        resumesInsertReqDto.setPortfolioSource("http://github.com/asdfqwer");
+
+        String filename = "p4.jpg";
+        Resource resource = loader.getResource("classpath:/static/images/" + filename);
+        MockMultipartFile file = new MockMultipartFile("file", "test.jpg", "image/jpg", resource.getInputStream());
+
+        String body = om.writeValueAsString(resumesInsertReqDto);
+        MockMultipartFile multipartBody = new MockMultipartFile("resumesInsertReqDto", "formData", APPLICATION_JSON,
+                body.getBytes());
+
+        System.out.println("디버그1 : " + body);
+        System.out.println("디버그2 : " + multipartBody);
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(multipart(HttpMethod.POST, "/s/resumes/insert")
+                        .file(file)
+                        .file(multipartBody)
+                        .accept(APPLICATION_JSON)
+                        .cookie(mockCookie)
+                        .session(session));
+
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(jsonPath("$.code").value(1));
+        resultActions.andExpect(jsonPath("$.message").value("이력서 등록 성공"));
+    }
+
+    @Test
+    public void findByResumesId_test() throws Exception {
+        // given
+        Integer id = 1;
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.get("/resumes/" + id).accept(APPLICATION_JSON));
+
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        resultActions.andExpect(jsonPath("$.code").value(1));
+        resultActions.andExpect(jsonPath("$.message").value("내 이력서 상세 보기 성공"));
+    }
 }
