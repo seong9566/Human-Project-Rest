81f294c14759320a39b0dbeeb40ba5f9c12f9ec2
seong9566 <dlgustjd9566@gmail.com>
2022-11-07 14:06:39 +0900

    web 아래 ResumesDto 삭제

--- src/main/java/site/metacoding/miniproject/web/UsersController.java
+++ src/main/java/site/metacoding/miniproject/web/UsersController.java
@@ -4,12 +4,10 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
