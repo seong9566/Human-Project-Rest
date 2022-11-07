d9dae7c11d39cca7d52f6c7e061e0056fa39cfbf
BeomyoonHeo <55969155+BeomyoonHeo@users.noreply.github.com>
2022-11-07 19:31:03 +0900

    [허범윤]Junit 구독 테스트 insert 완료 (#70)
    * 기업회원가입 Rest변경 완료
    
    * signeddto 위치변경 및 구조 변경
    
    * 로그인로직 임시로 완료
    
    * 로그인 컨트롤러에 세션정보 추가
    
    * SignDto 직렬화 & 역직렬화 적용
    
    * Jwt Token filter 입시저장
    
    * 토큰만료체크완료
    
    * 푸시전 머지 완료
    
    * 토큰검증 로직 에러 수정
    
    * 회원가입시 자동로그인 오류로직 수정완료
    
    * 쿠키관련 null에러 고침
    
    * 토큰으로 Dto생성 반환 완료
    
    * 쿠키 expired time 추가 및 메서드 네이밍 수정
    
    * 포트번호 및 메서드 수정
    
    * Alarm build pattern 생성 및 서비스 네이밍 재정의
    
    * 알람dto수정 및 쿠키 수명 연장
    
    * 알람 dto변경 완료
    
    * 임시저장
    
    * 유저 알람확인시 읽음표시 완료
    
    * 알람삭제 dto생성 진행중
    
    * 알람삭제 로직 완료
    
    * handler 수정
    
    * 컨트롤러 bean에러 발생
    
    * 마스터 pull 완료
    
    * readme 수정 및 sql파일 수정
    
    * h2데이터베이스 테스트 세팅 완료
    
    * 회사회원가임 테스트중
    
    * 테스트중
    
    * 인메모리데이터베이스 테이블 자동생성 완료
    
    * 기업회원가입 JUNIT테스트완료 multipart오류 해결
    
    * 로그인 테스트 완료
    
    * JUNIT 알람테스트로 쿠키값 테스트 완료
    
    * 임시저장
    
    * loginDto 위치변경
    
    * 컨트롤러 부 엔드포인트 /api추가 완료
    
    * 알람보기 테스트 완료
    
    * 쿠키패스 설정 완료
    
    * 유저알람 체크 테스트 완료
    
    * 임시저장
    
    * 유저알람 완료
    
    * 알람 null값 변경 및 구독로직 JUNIT테스트 완료

--- src/main/java/site/metacoding/miniproject/service/users/SubscribeService.java
+++ src/main/java/site/metacoding/miniproject/service/users/SubscribeService.java
@@ -9,12 +9,17 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.miniproject.domain.alarm.Alarm;
 import site.metacoding.miniproject.domain.alarm.AlarmDao;
+import site.metacoding.miniproject.domain.company.Company;
+import site.metacoding.miniproject.domain.company.CompanyDao;
 import site.metacoding.miniproject.domain.personal.Personal;
+import site.metacoding.miniproject.domain.personal.PersonalDao;
 import site.metacoding.miniproject.domain.subscribe.Subscribe;
 import site.metacoding.miniproject.domain.subscribe.SubscribeDao;
 import site.metacoding.miniproject.domain.users.Users;
 import site.metacoding.miniproject.domain.users.UsersDao;
+import site.metacoding.miniproject.dto.user.UserRespDto.SignPersonalDto;
 import site.metacoding.miniproject.dto.user.UserRespDto.SignedDto;
+import site.metacoding.miniproject.exception.ApiException;
 import site.metacoding.miniproject.utill.AlarmEnum;
 
 @Service
@@ -24,12 +29,26 @@ public class SubscribeService {
     private final SubscribeDao subscribeDao;
     private final AlarmDao alarmDao;
     private final UsersDao usersDao;
+    private final CompanyDao companyDao;
+    private final PersonalDao personalDao;
 
     @Transactional(rollbackFor = RuntimeException.class)
     public Subscribe subscribeToCompany(SignedDto<?> signedDto, Integer companyId) {
 
+        Company companyPS = companyDao.findById(companyId);
+        if (companyPS == null) {
+            throw new ApiException("해당 기업이 존재하지 않습니다.");
+        }
+
+
+
         HashMap<String, Integer> subscribes = new HashMap<>();
-        Personal personalInfo = (Personal) signedDto.getUserInfo();
+        SignPersonalDto personalInfo = (SignPersonalDto) signedDto.getUserInfo();
+        Personal personalPS = personalDao.findById(companyId);
+
+        if (personalPS == null) {
+            throw new ApiException("유저가 존재하지 않습니다.");
+        }
 
         Subscribe subscribe = new Subscribe(personalInfo.getPersonalId(), companyId);
         subscribeDao.insert(subscribe);
@@ -37,7 +56,7 @@ public class SubscribeService {
         Users users = usersDao.findByCompanyId(companyId);
         subscribes.put(AlarmEnum.ALARMSUBSCRIBEID.key(), subscribe.getSubscribeId());
 
-        Alarm alarm = new Alarm(users.getUsersId(), subscribes, personalInfo.getPersonalName());
+        Alarm alarm = new Alarm(users.getUsersId(), subscribes, personalPS.getPersonalName());
         alarmDao.insert(alarm);
 
         subscribe.setAlarmId(alarm.getAlarmId());
@@ -48,8 +67,10 @@ public class SubscribeService {
 
     @Transactional(rollbackFor = RuntimeException.class)
     public List<Subscribe> subscribeCancelToCompany(Integer subscribeId, SignedDto<?> signedDto) {
+        
+        SignPersonalDto personalInfo = (SignPersonalDto) signedDto.getUserInfo();
+        
         subscribeDao.deleteById(subscribeId);
-        Personal personalInfo = (Personal) signedDto.getUserInfo();
         List<Subscribe> subscribes = subscribeDao.findByPersonalId(personalInfo.getPersonalId());
         return subscribes;
 
