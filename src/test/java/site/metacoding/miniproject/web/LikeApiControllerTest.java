8a9479db702f5304d2d9f52d0388c4d709935a81
sopu55555 <sopu55555@naver.com>
2022-11-07 17:26:53 +0900

    커밋

--- src/test/java/site/metacoding/miniproject/web/LikeApiControllerTest.java
+++ src/test/java/site/metacoding/miniproject/web/LikeApiControllerTest.java
@@ -24,8 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import site.metacoding.miniproject.dto.like.LikeReqDto.PersonalLikeReqDto;
-import site.metacoding.miniproject.dto.user.UserRespDto.SignCompanyDto;
+import site.metacoding.miniproject.dto.user.UserRespDto.SignPersonalDto;
 import site.metacoding.miniproject.dto.user.UserRespDto.SignedDto;
 import site.metacoding.miniproject.utill.JWTToken.CreateJWTToken;
 
@@ -48,14 +47,29 @@ public class LikeApiControllerTest {
 
     }
 
-    @BeforeEach // test메서드 진입전에 트랜잭션 발동
+    // @BeforeEach // test메서드 진입전에 트랜잭션 발동
+    // public void sessionInit() {
+
+    // session = new MockHttpSession();
+    // SignCompanyDto signCompanyDto = new SignCompanyDto();
+
+    // signCompanyDto.setCompanyId(1);
+    // SignedDto<?> signedDto = new SignedDto<>(1, "testuser1", signCompanyDto);
+
+    // session.setAttribute("principal", signedDto);
+
+    // String JwtToken = CreateJWTToken.createToken(signedDto); // Authorization
+    // mockCookie = new MockCookie("Authorization", JwtToken);
+
+    // }
+    @BeforeEach
     public void sessionInit() {
 
         session = new MockHttpSession();
-        SignCompanyDto signCompanyDto = new SignCompanyDto();
+        SignPersonalDto signPersonalDto = new SignPersonalDto();
 
-        signCompanyDto.setCompanyId(1);
-        SignedDto<?> signedDto = new SignedDto<>(1, "testuser1", signCompanyDto);
+        signPersonalDto.setPersonalId(1);
+        SignedDto<?> signedDto = new SignedDto<>(1, "testuser1", signPersonalDto);
 
         session.setAttribute("principal", signedDto);
 
@@ -91,10 +105,10 @@ public class LikeApiControllerTest {
     @Sql(scripts = "classpath:testsql/insertuserforlike.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
     @Test
     public void deletePersonalLike_test() throws Exception {
-        Integer resumesId = 2;
+        Integer resumesId = 1;
 
         ResultActions resultActions = mvc
-                .perform(delete("/company/deleteSubscribe/" + resumesId)
+                .perform(delete("/s/api/personalLike/" + resumesId)
                         .accept(APPLICATION_JSON)
                         .session(session));
 
@@ -104,4 +118,38 @@ public class LikeApiControllerTest {
         System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
     }
 
+    @Sql(scripts = "classpath:testdatabase.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Test
+    public void insertCompanyLike_test() throws Exception {
+
+        // given
+        Integer companyId = 1;
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.post("/s/api/companyLike/" + companyId).session(session)
+                        .cookie(mockCookie)
+                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON));
+        System.out.println("디버그 : " + resultActions.andReturn().getResponse().getContentAsString());
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
+    }
+
+    @Sql(scripts = "classpath:testsql/insertuserforlike.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Test
+    public void deleteCompanyLike_test() throws Exception {
+        Integer companyId = 2;
+
+        ResultActions resultActions = mvc
+                .perform(delete("/s/api/companyLike/{companyId}" + companyId)
+                        .accept(APPLICATION_JSON)
+                        .session(session));
+
+        // then/ charset=utf-8안넣으면바로한글이깨진다
+
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+    }
 }
