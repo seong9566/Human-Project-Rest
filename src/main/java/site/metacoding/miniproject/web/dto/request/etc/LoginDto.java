b7112f5862e5a90cbe40adefd06887cd1e6f28ad
BeomyoonHeo <55969155+BeomyoonHeo@users.noreply.github.com>
2022-11-07 14:12:54 +0900

    [허범윤]LoginDto 위치 변경 (#55)
    * 기업회원가입 Rest변경 완료
    
    * signeddto 위치변경 및 구조 변경
    
    * 로그인로직 임시로 완료
    
    * 로그인 컨트롤러에 세션정보 추가
    
    * SignDto 직렬화 & 역직렬화 적용
    
    * Jwt Token filter 입시저장
    
    * 토큰만료체크완료
    
    * 푸시전 머지 완료
    
    * 토큰검증 로직 에러 수정
    
    * 회원가입시 자동로그인 오류로직 수정완료
    
    * 쿠키관련 null에러 고침
    
    * 토큰으로 Dto생성 반환 완료
    
    * 쿠키 expired time 추가 및 메서드 네이밍 수정
    
    * 포트번호 및 메서드 수정
    
    * Alarm build pattern 생성 및 서비스 네이밍 재정의
    
    * 알람dto수정 및 쿠키 수명 연장
    
    * 알람 dto변경 완료
    
    * 임시저장
    
    * 유저 알람확인시 읽음표시 완료
    
    * 알람삭제 dto생성 진행중
    
    * 알람삭제 로직 완료
    
    * handler 수정
    
    * 컨트롤러 bean에러 발생
    
    * 마스터 pull 완료
    
    * readme 수정 및 sql파일 수정
    
    * h2데이터베이스 테스트 세팅 완료
    
    * 회사회원가임 테스트중
    
    * 테스트중
    
    * 인메모리데이터베이스 테이블 자동생성 완료
    
    * 기업회원가입 JUNIT테스트완료 multipart오류 해결
    
    * 로그인 테스트 완료
    
    * JUNIT 알람테스트로 쿠키값 테스트 완료
    
    * 임시저장
    
    * loginDto 위치변경

--- src/main/java/site/metacoding/miniproject/web/dto/request/etc/LoginDto.java
+++ src/main/java/site/metacoding/miniproject/web/dto/request/etc/LoginDto.java
@@ -2,18 +2,3 @@ package site.metacoding.miniproject.web.dto.request.etc;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class LoginDto {
-	private String loginId;
-	private String loginPassword;
-
-
-	public LoginDto(String loginId, String loginPassword) {
-		this.loginId = loginId;
-		this.loginPassword = loginPassword;
-	}
-
-
-}
